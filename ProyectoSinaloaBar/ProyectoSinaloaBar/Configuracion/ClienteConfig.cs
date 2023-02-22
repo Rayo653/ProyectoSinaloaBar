03ddb015649d9f8d45ca1407844e6ba4a7e18d88
Alejandro0060 <yarodriguez1111@misena.edu.co>
2023-02-22 18:50:50 -0500

    Fluent Api con algunas Entidades
    Entidades con validaciones pero sin relaciones

---
+++ ProyectoSinaloaBar/ProyectoSinaloaBar/Configuracion/ClienteConfig.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ProyectoSinaloaBar.Models;
+
+namespace ProyectoSinaloaBar.Configuracion
+{
+    public class ClienteConfig : IEntityTypeConfiguration<Cliente>
+    {
+        public void Configure(EntityTypeBuilder<Cliente> builder)
+        {
+            builder.ToTable("Clientes");
+
+            builder.HasKey(c => c.IdCliente);
+
+            //builder.HasOne(c => c.Persona)
+            //       .WithOne()
+            //       .HasForeignKey<Cliente>(c => c.IdPersona);
+        }
+    }
+}
---
+++ ProyectoSinaloaBar/ProyectoSinaloaBar/Configuracion/ProveedorConfig.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ProyectoSinaloaBar.Models;
+
+namespace ProyectoSinaloaBar.Configuracion
+{
+    public class ProveedorConfig : IEntityTypeConfiguration<Proveedor>
+    {
+        public void Configure(EntityTypeBuilder<Proveedor> builder)
+        {
+            builder.ToTable("Proveedores");
+
+            builder.HasKey(c => c.IdProveedor);
+
+            //builder.HasOne(c => c.Persona)
+            //       .WithOne()
+            //       .HasForeignKey<Proveedor>(c => c.IdPersona);
+
+        }
+    }
+}
