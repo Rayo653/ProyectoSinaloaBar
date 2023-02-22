2aca45596ea506afe899101cdc2c7d3979c1db33
Alejandro0060 <yarodriguez1111@misena.edu.co>
2023-02-22 18:08:29 -0500

    Validacion API Fluent
    Se esta realizando la validacion y las relaciones de las entidades con API Fluent

--- ProyectoSinaloaBar/ProyectoSinaloaBar/Models/TipoDocumento.cs
+++ ProyectoSinaloaBar/ProyectoSinaloaBar/Models/TipoDocumento.cs
@@ -1,13 +1,21 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace ProyectoSinaloaBar.Models
 {
     public class TipoDocumento
     {
+
         
+    
         public int IdTdoc { get; set; }
+      
         public string Tdoc { get; set; }
+      
         public string DescTdoc { get; set; }
+
+
+        [DefaultValue(false)]
         public bool Estado { get; set; }
 
 
